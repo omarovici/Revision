0cae6b62ea4eda453d7e132aa365cd86a1050bc9
omarovici <omarkhalidsaber042004@gmail.com>
2025-09-06 20:04:51 +0300

    Remove unused Fun01 method and related code from Program.cs

--- Revision/Program.cs
+++ Revision/Program.cs
@@ -2,18 +2,7 @@
 
 class Program
 {
-    static Action Fun01()
-    {
-        Action action = () => Console.WriteLine("Hello World!");
-        return action;
-    }
     static void Main(string[] args)
     {
-        // Fun01().Invoke();
-        // Fun01()();
-        
-        
-        // var action = Fun01();
-        // action();
     }
 }
