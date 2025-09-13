d953c54f3c6a6572f90460c3747f896dfeb20a2e
omarovici <omarkhalidsaber042004@gmail.com>
2025-09-13 18:27:13 +0300

    dynamic

--- Revision/Program.cs
+++ Revision/Program.cs
@@ -8,16 +8,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Dictionary<string,long> dict = new Dictionary<string,long>();
-        dict.Add("FIFA", 2024);
-        dict.Add("YouTube", 2005);
-        dict.Add("Instagram", 2010);
-        dict.Add("Snapchat", 2011);
-        dict.Add("TikTok", 2016);
-        dict.Add("Facebook", 2004);
-        foreach (var item in dict)
-        {
-            Console.WriteLine($"{item.Key} was released in {item.Value}");
-        }
+        dynamic Data = "AHMED";
+        Console.WriteLine(Data);
+        Data = 100;
+        Console.WriteLine(Data);
+        Data = 99.99;
+        Console.WriteLine(Data);
+        Data = true;
+        Console.WriteLine(Data);
+        Data = null;
+        Console.WriteLine(Data);
     }
 }
--- Revision/obj/Debug/net9.0/Revision.AssemblyInfo.cs
+++ Revision/obj/Debug/net9.0/Revision.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Revision")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd411c9b2cde69e99a732f2801cfd065a085c564")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2eee1fb0cf984557accbe1e2598d0614457496d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Revision")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Revision")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
