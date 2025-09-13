a00d3acbff6a73508a049f1871bc2f396e1b0081
omarovici <omarkhalidsaber042004@gmail.com>
2025-09-13 18:31:41 +0300

    remove dynamic variable examples from Main method

--- Revision/Program.cs
+++ Revision/Program.cs
@@ -1,22 +1,10 @@
 ﻿using System.Collections;
-using Revision.FIFA_Game;
-using Revision.YouTube;
-
 namespace Revision;
 
 class Program
 {
     static void Main(string[] args)
     {
-        dynamic Data = "AHMED";
-        Console.WriteLine(Data);
-        Data = 100;
-        Console.WriteLine(Data);
-        Data = 99.99;
-        Console.WriteLine(Data);
-        Data = true;
-        Console.WriteLine(Data);
-        Data = null;
-        Console.WriteLine(Data);
+        
     }
 }
