92b890954ba3d53a09a7539d1dfa141789ae8ba3
omarovici <omarkhalidsaber042004@gmail.com>
2025-09-13 22:56:08 +0300

    Data Setup

--- Revision/Program.cs
+++ Revision/Program.cs
@@ -5,16 +5,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // LINQ execution ways
-        // 1. Immediate Execution
-        // 2. Deferred Execution
-        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        var Result = numbers.Where(n => n % 2 == 0).ToList();
-        numbers.AddRange(new List<int> { 11, 12, 13 });
-        foreach (var item in Result)
-        {
-            Console.WriteLine(item);
-        }
         
     }
 }
